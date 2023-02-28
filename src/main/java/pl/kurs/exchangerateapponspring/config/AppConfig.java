346fee822594f1256717c97540e5211e675ed631
RoKo159 <kowalski.robert159@gmail.com>
2023-02-28 19:56:20 +0100

    Added pom and application configuration

---
+++ src/main/java/pl/kurs/exchangerateapponspring/config/AppConfig.java
@@ -0,0 +1,9 @@
+package pl.kurs.exchangerateapponspring.config;
+
+public interface AppConfig {
+
+    String API_PAGE = "https://api.fastforex.io";
+    String ENDPOINT_WITH_PARAMETER = "/fetch-all?from=";
+    String PRIVATE_API_KEY = "&api_key=2346695427-85d30ab4a5-rpx748";
+
+}
